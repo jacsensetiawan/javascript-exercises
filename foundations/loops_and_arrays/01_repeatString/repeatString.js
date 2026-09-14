f2c89e7b5d2c2ed5357215f2f407d3c7c02f12a9
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-14 22:52:16 +0700

    fix: changing the loop method to manual

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,9 +1,11 @@
 const repeatString = function(string, num) {
-    if (num < 0){
-        return "ERROR";
+    if (num < 0) return "ERROR";
+    let result = "";
+    for(let i = 0; i < num; i++) {
+        result += string;
     }
-    
-    return string.repeat(3);
+
+    return result;
 };
 
 // Do not edit below this line
