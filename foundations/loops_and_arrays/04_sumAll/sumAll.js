e218d09b2dc45778bee5b6d8e9cca20c61701ef3
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-22 20:39:57 +0700

    refactor: calculate total sum using array reduce

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,6 +1,8 @@
 const sumAll = function(a, b) {
 const result = (Array.from({lenght: b - a + 1}, (_, index) => index + a));
-
+const total = result.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+}, 0)
 };
 
 // Do not edit below this line
