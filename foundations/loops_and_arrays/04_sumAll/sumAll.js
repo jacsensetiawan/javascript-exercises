dc473e01b51d0079b688cd851cae676e364aff63
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-22 21:32:30 +0700

    feat: add ; after if condition

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,15 +1,15 @@
 const sumAll = function(a, b) {
 if (typeof a !== "number" || typeof b !== "number") {
     return "ERROR";
-}
+};
 
 if (!Number.isInteger(a) || !Number.isInteger(b)) {
     return "ERROR";
-}
+};
 
 if (a < 0 || b < 0){
     return "ERROR"
-}
+};
 
 let max;
 let min;
