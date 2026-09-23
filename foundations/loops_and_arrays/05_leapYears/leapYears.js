8c892bb164dc505d1997315cea4d56a9170a52cc
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-23 20:21:49 +0700

    fix: correct leap year modulo check for 400

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(years) {
-if (years % 4 === 0 && !(years % 100 === 0) || years % 400) {
+if (years % 4 === 0 && !(years % 100 === 0) || years % 400 === 0) {
     return true;
 } else {
     return false;
