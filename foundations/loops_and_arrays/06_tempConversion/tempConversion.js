0d55bce14528ce14d1df5f85d18994326d62fb72
jacsensetiawan <jacsensetsdeveloper@gmail.com>
2026-09-23 21:10:44 +0700

    feat: add a formula to convert fahrenheit to celsius

--- foundations/loops_and_arrays/06_tempConversion/tempConversion.js
+++ foundations/loops_and_arrays/06_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  const C = 5 / 9 * (F - 32);
+  const rounded = Math.round (C * 10) / 10
+  return rounded;
 };
 
 const convertToFahrenheit = function() {
+  
 };
 
 // Do not edit below this line
